5de6fd5f06901ddb31fd23625be09bb661ea5ffc
Cameron Young <cameronslatton@gmail.com>
2021-02-23 23:00:51 -0600

    changed code so it can run
    The code can be ran to test the logic in main

--- Midnight/Midnight.cs
+++ Midnight/Midnight.cs
@@ -1,8 +1,37 @@
 ﻿using System;
-
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
 namespace Midnight
 {
-    public class Class1
+    class Midnight
     {
+        static int Main(string[] args)
+        {
+            // Create a root command with some options
+            var rootCommand = new RootCommand
+             {
+                new Option<string>(
+                    "--gallop",
+                    getDefaultValue: () => "42",
+                    description: "An option to scan the current directory and detect the content to process, run"),
+
+                new Option<string>(
+                    "--fly",
+                    description: "An option to test a site locally")
+
+            };
+
+            rootCommand.Description = "Midnight's test run";
+
+            // Note that the parameters of the handler method are matched according to the names of the options
+            rootCommand.Handler = CommandHandler.Create<string>((stringOption) =>
+                {
+                    Console.WriteLine($"The value for --gallop is: {stringOption}");
+                });
+
+            // Parse the incoming args and invoke the handler
+            return rootCommand.InvokeAsync(args).Result;
+        }
     }
 }
