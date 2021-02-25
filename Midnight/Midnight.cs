499ff0eaf9b7ee60a8008fc066e5b46b77b10d54
Cameron Young <cameronslatton@gmail.com>
2021-02-24 21:38:49 -0600

    Solved the binding issue.

--- Midnight/Midnight.cs
+++ Midnight/Midnight.cs
@@ -6,32 +6,31 @@ namespace Midnight
 {
     class Midnight
     {
-        static int Main(string[] args)
-        {
-            // Create a root command with some options
-            var rootCommand = new RootCommand
-             {
-                new Option<string>(
-                    "--gallop",
-                    getDefaultValue: () => "42",
-                    description: "An option to scan the current directory and detect the content to process, run"),
+       static int Main(string[] args)
+{
+    // Create a root command with some options
+    var rootCommand = new RootCommand
+    {
+        new Option<string>(
+            "--gallop",
+            getDefaultValue: () => "42",
+            description: "An option whose argument is parsed as an int"),
+        new Option<string>(
+            "--fly",
+            "An option whose argument is parsed as a bool")
+    };
 
-                new Option<string>(
-                    "--fly",
-                    description: "An option to test a site locally")
+    rootCommand.Description = "My sample app";
 
-            };
+   // Note that the parameters of the handler method are matched according to the names of the options
+   rootCommand.Handler = CommandHandler.Create<string, string>((gallop, fly) =>
+    {
+        Console.WriteLine($"The value for --gallop is: {gallop}");
+        Console.WriteLine($"The value for --fly is: {fly}");
+    });
 
-            rootCommand.Description = "Midnight's test run";
-
-            // Note that the parameters of the handler method are matched according to the names of the options
-            rootCommand.Handler = CommandHandler.Create<string>((stringOption) =>
-                {
-                    Console.WriteLine($"The value for --gallop is: {stringOption}");
-                });
-
-            // Parse the incoming args and invoke the handler
-            return rootCommand.InvokeAsync(args).Result;
-        }
+    // Parse the incoming args and invoke the handler
+    return rootCommand.InvokeAsync(args).Result;
+}
     }
 }
