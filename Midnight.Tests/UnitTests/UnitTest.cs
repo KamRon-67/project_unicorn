84d57dce2da06be8766e7e85c6f4cd59ee2f86b1
Cameron Young <cameronslatton@gmail.com>
2021-02-24 21:59:22 -0600

    Add a simple test
    Adding code coverage.

--- Midnight.Tests/UnitTests/UnitTest.cs
+++ Midnight.Tests/UnitTests/UnitTest.cs
@@ -1,20 +1,22 @@
 using System;
 using Midnight.Core.Extensions;
 using Xunit;
+using FluentAssertions;
+using System.CommandLine;
 
 namespace Midnight.Tests.UnitTests
 {
     public class UnitTest
     {
-         [Fact]
-            public void Parse_multiple_fields_in_header()
-            {
-                const string header = @"---
+        [Fact]
+        public void Parse_multiple_fields_in_header()
+        {
+            const string header = @"---
                         layout: post
                         title: This is a test jekyll document
                         description: Test YamlHeader method
                         date: 2021-01-30
-                        tags : 
+                        tags :
                         - test
                         - alsotest
                         - lasttest
@@ -24,12 +26,30 @@ namespace Midnight.Tests.UnitTests
 
                         This is a test of YAML parsing";
 
-                var result = header.YamlHeader();
+            var result = header.YamlHeader();
 
-                Assert.Equal("post", result["layout"].ToString());
-                Assert.Equal("This is a test jekyll document", result["title"].ToString());
-                Assert.Equal("2021-01-30", result["date"].ToString());
-                Assert.Equal("[ test, alsotest, lasttest ]", result["tags"].ToString());
-            }
+            Assert.Equal("post", result["layout"].ToString());
+            Assert.Equal("This is a test jekyll document", result["title"].ToString());
+            Assert.Equal("2021-01-30", result["date"].ToString());
+            Assert.Equal("[ test, alsotest, lasttest ]", result["tags"].ToString());
+        }
+
+        [Fact]
+        public void When_multiple_options_are_configured_then_they_must_differ_by_name()
+        {
+            var command = new Command("the-command")
+            {
+                new Option("--same")
+            };
+
+            command
+                .Invoking(c => c.Add(new Option("--same")))
+                .Should()
+                .Throw<ArgumentException>()
+                .And
+                .Message
+                .Should()
+                .Be("Alias '--same' is already in use.");
+        }
     }
 }
