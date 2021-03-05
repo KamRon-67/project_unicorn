cf7df12fef7c0025f68a40739b4b19b8122eb025
Cameron Young <cameronslatton@gmail.com>
2021-03-04 22:56:43 -0600

    Moving to file generation
    Start on files and folders.

---
+++ Midnight/Core/Extensions/UnicornEngine.cs
@@ -0,0 +1,27 @@
+﻿using Midnight.Core.Extensions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midnight.Core.Extensions
+{
+    public class UnicornEngine : ITemplateEngine
+    {
+        public void Process(IFileSystem fileSystem, string folder)
+        {
+            var outputPath = Path.Combine(folder, "_site");
+            fileSystem.Directory.CreateDirectory(outputPath);
+
+            foreach (var file in fileSystem.Directory.GetFiles(folder))
+            {
+                var fileName = Path.GetFileName(file);
+                var newPath = Path.Combine(outputPath, fileName);
+                fileSystem.File.WriteAllText(newPath, fileSystem.File.ReadAllText(file));
+            }
+        }
+
+    }
+}
