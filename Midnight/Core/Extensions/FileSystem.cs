2201bf3e9aa44a6fd15647d37965feda22ff2c78
Cameron Young <Cameronslatton@gmail.com>
2021-03-14 13:36:02 -0500

    Project now compiles

---
+++ Midnight/Core/Extensions/FileSystem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Linq;
+using Midnight.Core.Extensions.Interfaces;
+using Midnight.Core.Features.Shared;
+using System.IO;
+using File = Midnight.Core.Features.FileSystem.File;
+using System.Threading.Tasks;
+
+namespace Midnight.Core.Extensions
+{
+    public class FileSystem : IUnicornFileSystem
+    {
+        public ServiceResult<IEnumerable<File>> GetFiles(string path)
+        {
+            try
+            {
+                var directory = new DirectoryInfo(path);
+                var files = directory.EnumerateFiles("*.*", SearchOption.AllDirectories).Select(this.MapFileInfoToFile);
+                return new ServiceResult<IEnumerable<File>> { Data = files };
+            }
+
+            catch(Exception e)
+            {
+                return new ServiceResult<IEnumerable<File>>(e.Message);
+            }
+        }
+
+        public async Task<ServiceResult<bool>> WriteFilesAsync(List<File> files)
+        {
+            return new ServiceResult<bool>();
+        }
+
+
+        private File MapFileInfoToFile(FileInfo info)
+        {
+            return new File()
+            {
+                Name = Path.GetFileNameWithoutExtension(info.Name),
+                Extension = info.Extension,
+                Directory = info.DirectoryName
+            };
+        }
+        
+    }
+}
--- Midnight/Core/Extensions/Interfaces/IUnicornFileSystem.cs
+++ Midnight/Core/Extensions/Interfaces/IUnicornFileSystem.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using System.IO.Abstractions;
 using System.Threading.Tasks;
 using System;
 using System.Linq;
 using SystemFile = System.IO.File;
+using Midnight.Core.Features.FileSystem;
+using Midnight.Core.Features.Shared;
 
 namespace Midnight.Core.Extensions.Interfaces
 {
-    public interface IUnicornFileSystem : IFileSystem
+    public interface IUnicornFileSystem
     {
         ServiceResult<IEnumerable<File>> GetFiles(string path);
         Task<ServiceResult<bool>> WriteFilesAsync(List<File> files);
