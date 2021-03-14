1df78252b02dda636a645ca302949bad038a557b
Cameron Young <Cameronslatton@gmail.com>
2021-03-14 15:21:01 -0500

    Refactor the main method

--- Midnight/Midnight.cs
+++ Midnight/Midnight.cs
@@ -2,6 +2,7 @@
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.Net;
+using System.Threading.Tasks;
 using Midnight.Core.Models;
 using Midnight.Core.Server;
 
@@ -17,59 +18,63 @@ namespace Midnight
             rootCommand.Description = "Simple static site generator";
 
             // Note that the parameters of the handler method are matched according to the names of the options
-            rootCommand.Handler = CommandHandler.Create<string, string>((create, generate) =>
-             {
-                 Console.WriteLine($"The value for --create is: {create}");
-                 Console.WriteLine($"The value for --generate is: {generate}");
-
-                 // HTTP server port
-                 int port = 8080;
-
-                 // HTTP server content path
-                 string www = "../../../www";
-
-
-                 Console.WriteLine($"HTTP server port: {port}");
-                 Console.WriteLine($"HTTP server static content path: {www}");
-                 Console.WriteLine($"HTTP server website: http://localhost:{port}/api/index.html");
-
-                 Console.WriteLine();
-
-                 // Create a new HTTP server
-                 var server = new HttpCacheServer(IPAddress.Any, port);
-                 server.AddStaticContent(www, "/api");
-
-                 // Start the server
-                 Console.Write("Server starting...");
-                 server.Start();
-                 Console.WriteLine("Done!");
-
-                 Console.WriteLine("Press Enter to stop the server or '!' to restart the server...");
-
-                 //Perform text input
-                 for (; ; )
-                 {
-                     string line = Console.ReadLine();
-                     if (string.IsNullOrEmpty(line))
-                         break;
-
-                     // Restart the server
-                     if (line == "!")
-                     {
-                         Console.Write("Server restarting...");
-                         server.Restart();
-                         Console.WriteLine("Done!");
-                     }
-                 }
-
-                 // Stop the server
-                 Console.Write("Server stopping...");
-                 server.Stop();
-                 Console.WriteLine("Done!");
-             });
+            rootCommand.Handler = CommandHandler.Create<string, string>(DoSomething);
 
             // Parse the incoming args and invoke the handler
             return rootCommand.InvokeAsync(args).Result;
+        }
+
+        public static void DoSomething(string create, string generate)
+        {
+            Console.WriteLine($"The value for --create is: {create}");
+            Console.WriteLine($"The value for --generate is: {generate}");
+
+            // HTTP server port
+            int port = 8080;
+
+            // HTTP server content path
+            string www = "../../../www";
+
+
+            Console.WriteLine($"HTTP server port: {port}");
+            Console.WriteLine($"HTTP server static content path: {www}");
+            Console.WriteLine($"HTTP server website: http://localhost:{port}/api/index.html");
+
+            Console.WriteLine();
+
+            // Create a new HTTP server
+            var server = new HttpCacheServer(IPAddress.Any, port);
+            server.AddStaticContent(www, "/api");
+
+            // Start the server
+            Console.Write("Server starting...");
+            server.Start();
+            Console.WriteLine("Done!");
+
+            Console.WriteLine("Press Enter to stop the server or '!' to restart the server...");
+
+
+
+            //Perform text input
+            for (; ; )
+            {
+                string line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+
+                // Restart the server
+                if (line == "!")
+                {
+                    Console.Write("Server restarting...");
+                    server.Restart();
+                    Console.WriteLine("Done!");
+                }
+            }
+
+            // Stop the server
+            Console.Write("Server stopping...");
+            server.Stop();
+            Console.WriteLine("Done!");
         }
     }
 }
