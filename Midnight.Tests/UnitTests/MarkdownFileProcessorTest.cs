72d087fecbf92bb841bd1e2351fc0fcd2d58429d
Cameron Young <Cameronslatton@gmail.com>
2021-03-24 23:23:17 -0500

    set up tests
    Set up test mocking

--- Midnight.Tests/UnitTests/MarkdownFileProcessorTest.cs
+++ Midnight.Tests/UnitTests/MarkdownFileProcessorTest.cs
@@ -8,12 +8,28 @@ using System.Collections.Generic;
 using Midnight.Core.Extensions.Models;
 using Midnight.Core.Extensions.Interfaces;
 using System.IO;
-using System.Reflection;
+using System.IO.Abstractions;
+using AutoFixture;
+using System.Threading.Tasks;
 
 namespace Midnight.Tests.UnitTests
 {
     public class MarkdownFileProcessorTest
     {
+        readonly Mock<IUnicornFileSystem> _fileSystem;
+        readonly Fixture _fixture;
+
+        public MarkdownFileProcessorTest()
+        {
+            _fixture = new Fixture();
+            _fileSystem = new Mock<IUnicornFileSystem>();
+            _fileSystem.Setup(f => f.GetFiles(It.IsAny<String>())).Verifiable();
+            _fileSystem.Setup(f => f.ReadAllTextAsync(It.IsAny<string>())).Returns(Task.CompletedTask as Task<string>);
+            _fileSystem.Setup(f => f.WriteOutputFilesAsync(It.IsAny<IEnumerable<OutputFile>>())).Returns(Task.CompletedTask as Task);
+            _fileSystem.Setup(f => f.WriteOutputFileAsync(It.IsAny<OutputFile>())).Returns(Task.CompletedTask as Task);
+
+        }
+
         [Fact]
         public void testGetFilesEmptyFolder()
         {
@@ -26,6 +42,11 @@ namespace Midnight.Tests.UnitTests
 
             //Assert
             result.Should().NotBeEmpty();
+        }
+
+        public void testProcessInputAsync()
+        {
+            //Arrange
         }
     }
 }
