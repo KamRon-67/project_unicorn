5bf12e692316116d037c636cb1a8e9f6c556d76e
Cameron Young <Cameronslatton@gmail.com>
2021-04-01 21:31:25 -0500

    Unit tests
    More unit tests

--- Midnight.Tests/UnitTests/UnicornEngineTest.cs
+++ Midnight.Tests/UnitTests/UnicornEngineTest.cs
@@ -1,16 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Midnight.Core.Extensions;
+using Xunit;
+using FluentAssertions;
+using Midnight.Core.Extensions.Models;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using Midnight.Core.Extensions.Interfaces;
+using System;
+using Moq;
 
 namespace Midnight.Tests.UnitTests
 {
     public class UnicornEngineTest
     {
-        public UnicornEngineTest()
+        [Fact]
+        public void UnicornEngineThrowException()
         {
+            //Arrange
+            Action testUnicornEngine = () => { _ = new UnicornEngine(null, null, null); };
 
+            //Act
+            var ex = Record.Exception(testUnicornEngine);
+            Assert.NotNull(ex);
+
+            // Assert
+            Assert.IsType<ArgumentException>(ex);
         }
     }
 }
